415dd922c51c55966c344ff1438d0c3086d094a8
rubel04 <alomrana886@gmail.com>
2025-04-30 15:06:08 +0600

    connect to mongodb database

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient, ServerApiVersion } from "mongodb";
+import "dotenv/config"
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0goom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+async function run() {
+  try {
+    await client.connect();
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Your partner is finding");
