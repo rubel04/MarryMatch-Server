08d6870b6bb521c3a5f8051880ae353e44bffa4b
rubel04 <alomrana886@gmail.com>
2025-05-02 07:30:59 +0600

    load specific biodata by id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { MongoClient, ServerApiVersion } from "mongodb";
-import "dotenv/config"
+import "dotenv/config";
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,16 +9,15 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0goom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true
-    }
-})
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 async function run() {
   try {
@@ -27,15 +26,31 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-
     // create bio data collection
     const bioDataCollection = client.db("marryMatchDB").collection("biodatas");
 
-    // get member bio data
+    // get member bio data base of age ascending
     app.get("/premium-member", async (req, res) => {
-      const bioData = await bioDataCollection.find().toArray();
+      const sortByAge = req.query.sort;
+      // console.log(sortByAge);
+      let sortOption = { age: 1 };
+      if (sortByAge === "dsc") {
+        sortOption = { age: -1 };
+      }
+      const bioData = await bioDataCollection.find().sort(sortOption).toArray();
       res.send(bioData);
+    });
+
+    // get single biodata details
+    app.get("/biodata/:id", async (req, res) => {
+      const id = req.params.id;
+      const bioId = parseInt(id);
+      const query = { biodataId: bioId };
+      const biodata = await bioDataCollection.findOne(query);
+      res.send(biodata);
     })
+
+
 
   } finally {
     // await client.close();
